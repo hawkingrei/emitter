5aae0afce3e2ec9802ea549ccc6b2a33a70d1f36
真人 <weizhen.wang@duitang.com>
2017-08-20 15:23:36 +0800

    first commit

---
+++ test/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/hawkingrei/emitter"
+)
+
+type SpecialString interface{}
+type Year interface{}
+
+type Other struct {
+	Id int
+}
+
+func (o *Other) One(output component.Output) {
+	out := reflect.ValueOf(output)
+	for a := range []int{2, 3, 4} {
+		fmt.Println("one ", a)
+		v := reflect.ValueOf(a)
+		out.Send(v)
+	}
+}
+
+func (o *Other) Two(a int) {
+	fmt.Println(a + o.Id)
+}
+
+func main() {
+	wf := component.NewWorkflow()
+	one := Other{Id: 1}
+	inj := inject.New()
+	wf.Add(one.One, inj, reflect.TypeOf(1))
+	wf.Add(one.Two, inj, reflect.TypeOf(1))
+	wf.Run()
+}
---
+++ wait_group_wrapper.go
@@ -0,0 +1,26 @@
+package emitter
+
+import (
+	"reflect"
+	"sync"
+)
+
+type WaitGroupWrapper struct {
+	sync.WaitGroup
+}
+
+func (w *WaitGroupWrapper) ReflectWrap(f reflect.Value, p []reflect.Value) {
+	w.Add(1)
+	go func() {
+		f.Call(p)
+		w.Done()
+	}()
+}
+
+func (w *WaitGroupWrapper) Wrap(cb func()) {
+	w.Add(1)
+	go func() {
+		cb()
+		w.Done()
+	}()
+}
